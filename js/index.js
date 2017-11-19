09d29c9b89836246333ca0690e1ee70a44213319
Vedarth <vedarth.sharma@gmail.com>
2017-11-19 18:55:16 +0530

    Removed inline css from index

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+            $(document).foundation();
+         })
+         $('.responsive1').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    }
+  ]
+});
